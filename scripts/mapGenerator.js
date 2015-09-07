d5fb2c585b653799a8951da087af067b0c2b2252
ChexWarrior <aplehm@gmail.com>
2015-09-07 16:39:14 -0400

    Remove unused methods Add beginnings of plan

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -11,44 +11,41 @@ document.addEventListener("DOMContentLoaded", function () {
       minAmtPoints: 4,
       maxAmtPoints: 10
     };
-    createShape(250, 250, 8, 50, 300);
+    createShape(250, 250, 25, 100, 200);
 });
-
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
-
-    return this;
-}
 
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = angleInDegrees * Math.PI / 180.0;
-  var x = centerX + radius * Math.cos(angleInRadians);
-  var y = centerY + radius * Math.sin(angleInRadians);
-  console.log("X: " + x + " Y: " + y);
+  var angleInRadians = angleInDegrees * Math.PI / 180.0,
+      x = centerX + radius * Math.cos(angleInRadians),
+      y = centerY + radius * Math.sin(angleInRadians);
+  //console.log("X: " + x + " Y: " + y);
   return x + ", " + y;
 }
 
+//TODO: Need to tweak length so that it gradually increases or decreases...
+//
 function createShape(bCircleCenterX, bCircleCenterY, numSides, minDistance, maxDistance) {
   var polygon = "",
-    angleIncrement = parseInt(360 / numSides),
-    angle = 0,
-    length = 0,
-    index;
+      angleIncrement = 360 / numSides,
+      angle = 0,
+      length = 0,
+      index;
+  console.log("Angle Increment: " + angleIncrement);
   for(index = 0; index < numSides; index += 1) {
     length = generateRandomNumber(minDistance, maxDistance);
+    console.log("Length: " + length + " Angle: " + angle);
     polygon += polarToCartesian(bCircleCenterX, bCircleCenterY, length, angle) + ",";
     angle += angleIncrement;
     //angle += generateRandomNumber(angle, angle + angleIncrement);
   }
-  console.log("Polygon: " + polygon);
+  //console.log("Polygon: " + polygon);
   var shape = globals.boardElement.polygon(polygon.substr(0, polygon.lastIndexOf(",")));
   shape.attr({
-    fill: "red",
+    fill: "gray",
     stroke: "black"
   });
 }
