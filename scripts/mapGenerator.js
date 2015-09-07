64439adc1591bc6b4d3dfe3268c64267570c5e56
ChexWarrior <aplehm@gmail.com>
2015-09-07 18:47:38 -0400

    More tweaking shape generation....

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
       minAmtPoints: 4,
       maxAmtPoints: 10
     };
-    createShape(250, 250, 25, 100, 200);
+    createShape(250, 250, 35, 100, 300);
 });
 
 function generateRandomNumber(min, max) {
@@ -27,17 +27,43 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
 }
 
 //TODO: Need to tweak length so that it gradually increases or decreases...
-//
+// When determining length take previous length and determine it's distance to each min
+// and max limits. If closer to minimum chance of being closer to min should increase and
+// vice versa for the maximum...
+function determineLengthFactor(min, max, prevLength) {
+  var minDistance = prevLength - min,
+      maxDistance = Math.abs(prevLength - max),
+      halfDistance = (max - min) / 2;
+      if(minDistance > maxDistance) {
+        return [min, prevLength];
+      } else if(maxDistance > minDistance) {
+        return [prevLength, max];
+      } else {
+        return [min, max];
+      }
+} 
+
 function createShape(bCircleCenterX, bCircleCenterY, numSides, minDistance, maxDistance) {
   var polygon = "",
       angleIncrement = 360 / numSides,
       angle = 0,
       length = 0,
+      prevLength = 0,
+      currMax = maxDistance,
+      currMin = minDistance,
+      distance = [],
       index;
-  console.log("Angle Increment: " + angleIncrement);
+  //console.log("Angle Increment: " + angleIncrement);
   for(index = 0; index < numSides; index += 1) {
-    length = generateRandomNumber(minDistance, maxDistance);
-    console.log("Length: " + length + " Angle: " + angle);
+    if(prevLength) {
+      distance = determineLengthFactor(minDistance, maxDistance, prevLength);
+      currMin = distance[0];
+      currMax = distance[1];
+    } 
+    length = generateRandomNumber(currMin, currMax);
+    prevLength = length;
+    //console.log("Length: " + length + " Angle: " + angle);
+    console.log("Current Min: " + currMin + " Current Max: " + currMax + " Length: " + length);
     polygon += polarToCartesian(bCircleCenterX, bCircleCenterY, length, angle) + ",";
     angle += angleIncrement;
     //angle += generateRandomNumber(angle, angle + angleIncrement);
