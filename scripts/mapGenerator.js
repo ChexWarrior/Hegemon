d125eb6ac8d057e66e82ef3fe6b4db244407cb88
ChexWarrior <aplehm@gmail.com>
2015-09-07 11:06:49 -0400

    Refactor createShape to take in parametersS

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -1,18 +1,17 @@
 var globals = null;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     globals = {
-        boardElement: Snap("#map"),
-        boardWidth: 500,
-        boardHeight: 500,
-        shapeMinWidth: 100,
-        shapeMinHeight: 100,
-        boardPadding: 10,
-        minAmtPoints: 4,
-        maxAmtPoints: 10
+      boardElement: Snap("#map"),
+      boardWidth: 500,
+      boardHeight: 500,
+      shapeMinWidth: 100,
+      shapeMinHeight: 100,
+      boardPadding: 10,
+      minAmtPoints: 4,
+      maxAmtPoints: 10
     };
-    createShape();
-   // polarToCartesian(250, 250, 50, 15);
+    createShape(250, 250, 8, 50, 300);
 });
 
 function Point(x, y) {
@@ -26,25 +25,30 @@ function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createShape() {
-    var circle = globals.boardElement.circle(250, 250, 50);
-    circle.attr({
-        fill: "white",
-        stroke: "black"
-    });
-    var poly = polarToCartesian(250, 250, 50, 0) + 
-        ", " + polarToCartesian(250, 250, 50, 65);
-    var line = globals.boardElement.polyline(poly);
-    line.attr({
-        stroke: "red"
-    });
-}
-
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   var angleInRadians = angleInDegrees * Math.PI / 180.0;
   var x = centerX + radius * Math.cos(angleInRadians);
   var y = centerY + radius * Math.sin(angleInRadians);
   console.log("X: " + x + " Y: " + y);
-  //return [x,y];
   return x + ", " + y;
+}
+
+function createShape(bCircleCenterX, bCircleCenterY, numSides, minDistance, maxDistance) {
+  var polygon = "",
+    angleIncrement = parseInt(360 / numSides),
+    angle = 0,
+    length = 0,
+    index;
+  for(index = 0; index < numSides; index += 1) {
+    length = generateRandomNumber(minDistance, maxDistance);
+    polygon += polarToCartesian(bCircleCenterX, bCircleCenterY, length, angle) + ",";
+    angle += angleIncrement;
+    //angle += generateRandomNumber(angle, angle + angleIncrement);
+  }
+  console.log("Polygon: " + polygon);
+  var shape = globals.boardElement.polygon(polygon.substr(0, polygon.lastIndexOf(",")));
+  shape.attr({
+    fill: "red",
+    stroke: "black"
+  });
 }
