24e1f4d664ac191bd019dab7643d4c90f9dc7794
ChexWarrior <aplehm@gmail.com>
2015-10-26 19:32:54 -0400

    Refactor and reorganize

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -2,15 +2,15 @@ var globals = null;
 
 document.addEventListener("DOMContentLoaded", function () {
     globals = {
-      boardElement: Snap("#map"),
-      boardWidth: 500,
-      boardHeight: 500,
-      shapeMinWidth: 100,
-      shapeMinHeight: 100,
-      boardPadding: 10,
-      minAmtPoints: 4,
-      maxAmtPoints: 10,
-      marks: []
+        boardElement: Snap("#map"),
+        boardWidth: 500,
+        boardHeight: 500,
+        shapeMinWidth: 100,
+        shapeMinHeight: 100,
+        boardPadding: 10,
+        minAmtPoints: 4,
+        maxAmtPoints: 10,
+        marks: []
     };
     createShape(250, 250, 10, 100, 250);
 });
@@ -20,77 +20,53 @@ function generateRandomNumber(min, max) {
 }
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = angleInDegrees * Math.PI / 180.0,
-  x = centerX + radius * Math.cos(angleInRadians),
-  y = centerY + radius * Math.sin(angleInRadians);
-  //console.log("X: " + x + " Y: " + y);
-  globals.marks.push({
-    x: x,
-    y: y
-  });
-  return x + ", " + y;
+    var angleInRadians = angleInDegrees * Math.PI / 180.0,
+    x = centerX + radius * Math.cos(angleInRadians),
+    y = centerY + radius * Math.sin(angleInRadians);
+    //console.log("X: " + x + " Y: " + y);
+    // globals.marks.push({
+    //   x: x,
+    //   y: y
+    // });
+    return x + ", " + y;
 }
 
-//TODO: Need to tweak length so that it gradually increases or decreases...
-// When determining length take previous length and determine it"s distance to each min
-// and max limits. If closer to minimum chance of being closer to min should increase and
-// vice versa for the maximum...
-function determineLengthFactor(min, max, prevLength) {
-  var minDistance = prevLength - min,
-      maxDistance = Math.abs(prevLength - max),
-      halfDistance = (max - min) / 2;
-      if(minDistance > maxDistance) {
-        return [min, prevLength];
-      } else if(maxDistance > minDistance) {
-        return [prevLength, max];
-      } else {
-        return [min, max];
-      }
+//TODO: Need a way to gradually increase and decreaes heights between points
+function graduateLength(min, max, prevLength) {
+
 } 
 
-function createShape(bCircleCenterX, bCircleCenterY, numSides, minDistance, maxDistance) {
-  var polygon = "",
-      angleIncrement = 360 / numSides,
-      angle = 0,
-      length = 0,
-      a = 0,
-      b = 0,
-      prevLength = 0,
-      currMax = maxDistance,
-      currMin = minDistance,
-      distance = [],
-      index;
-  //console.log("Angle Increment: " + angleIncrement);
-  console.log("x: " + bCircleCenterX + " y:" + bCircleCenterY);
-  for(index = 0; index < numSides; index += 1) {
-    length = generateRandomNumber(minDistance, maxDistance);      
-    // console.log("Length: " + length + " Angle: " + angle);
-    // console.log("Current Min: " + currMin + " Current Max: " + currMax + " Length: " + length);
-    polygon += polarToCartesian(bCircleCenterX, bCircleCenterY, length, angle) + ",";
-    // if(prevLength) {
-    //   distance = determineLengthFactor(minDistance, maxDistance, prevLength);
-    //   currMin = distance[0];
-    //   currMax = distance[1];
-    // } 
-    length = generateRandomNumber(currMin, currMax);
-    prevLength = length;
-    //console.log("Length: " + length + " Angle: " + angle);
-    console.log("Current Min: " + currMin + " Current Max: " + currMax + " Length: " + length);
-    polygon += polarToCartesian(bCircleCenterX, bCircleCenterY, length, angle) + ",";
-    angle += angleIncrement;
-    //angle += generateRandomNumber(angle, angle + angleIncrement);
-  }
-  console.log("Polygon: " + polygon);
-  var shape = globals.boardElement.polygon(polygon.substr(0, polygon.lastIndexOf(",")));
-  shape.attr({
-    fill: "gray",
-    stroke: "black"
-  });
-  for(index = 0; index < globals.marks.length; index += 1) {
-    var mark = globals.boardElement.text(globals.marks[index].x, globals.marks[index].y, index);
-    mark.attr({
-      fill: "red",
-      stroke: "black"
+function createShape(centerXPos, centerYPos, numSides, minDistance, maxDistance) {
+var polygon = "",
+    angleIncrement = 360 / numSides,
+    angle = 0,
+    randomLength = 0,
+    prevLength = 0,
+    currMax = maxDistance,
+    currMin = minDistance,
+    distance = [],
+    index;
+    console.log("x: " + centerXPos + " y:" + centerYPos);
+    for(index = 0; index < numSides; index += 1) {
+        randomLength = generateRandomNumber(minDistance, maxDistance);      
+        polygon += polarToCartesian(centerXPos, centerYPos, randomLength, angle) + ",";
+        randomLength = generateRandomNumber(currMin, currMax);
+        prevLength = randomLength;
+        console.log("Current Min: " + currMin + " Current Max: " + currMax + " Length: " + randomLength);
+        polygon += polarToCartesian(centerXPos, centerYPos, randomLength, angle) + ",";
+        angle += angleIncrement;
+    }
+    console.log("Polygon: " + polygon);
+    var shape = globals.boardElement.polygon(polygon.substr(0, polygon.lastIndexOf(",")));
+        shape.attr({
+        fill: "gray",
+        stroke: "black"
     });
-  }
+    // for(index = 0; index < globals.marks.length; index += 1) {
+    //   var mark = globals.boardElement.text(globals.marks[index].x, globals.marks[index].y, index);
+    //   mark.attr({
+    //     fill: "red",
+    //     stroke: "black"
+    //   });
+    // }
 }
