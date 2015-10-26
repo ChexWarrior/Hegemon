505ccf9fb169a7eb7a5e8849c00841eb774b4564
ChexWarrior <aplehm@gmail.com>
2015-10-26 19:58:44 -0400

    Change code to use Path instead of Polygon

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -12,7 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
         maxAmtPoints: 10,
         marks: []
     };
-    createShape(250, 250, 10, 100, 250);
+    createShape(
+        globals.boardWidth / 2, 
+        globals.boardHeight / 2, 
+        30, 
+        100, 
+        200
+    );
 });
 
 function generateRandomNumber(min, max) {
@@ -23,50 +29,45 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians = angleInDegrees * Math.PI / 180.0,
     x = centerX + radius * Math.cos(angleInRadians),
     y = centerY + radius * Math.sin(angleInRadians);
-    //console.log("X: " + x + " Y: " + y);
-    // globals.marks.push({
-    //   x: x,
-    //   y: y
-    // });
-    return x + ", " + y;
+    
+    return x + " " + y;
 }
 
 //TODO: Need a way to gradually increase and decreaes heights between points
+//
 function graduateLength(min, max, prevLength) {
 
 } 
 
 function createShape(centerXPos, centerYPos, numSides, minDistance, maxDistance) {
-var polygon = "",
-    angleIncrement = 360 / numSides,
-    angle = 0,
-    randomLength = 0,
-    prevLength = 0,
-    currMax = maxDistance,
-    currMin = minDistance,
-    distance = [],
-    index;
+    var pathArray = [],
+        pathString = "",
+        angleIncrement = 360 / numSides,
+        angle = 0,
+        randomLength = 0,
+        prevLength = 0,
+        currMax = maxDistance,
+        currMin = minDistance,
+        index;
     console.log("x: " + centerXPos + " y:" + centerYPos);
     for(index = 0; index < numSides; index += 1) {
         randomLength = generateRandomNumber(minDistance, maxDistance);      
-        polygon += polarToCartesian(centerXPos, centerYPos, randomLength, angle) + ",";
+        pathArray[index] = polarToCartesian(centerXPos, centerYPos, randomLength, angle) + ",";
         randomLength = generateRandomNumber(currMin, currMax);
         prevLength = randomLength;
         console.log("Current Min: " + currMin + " Current Max: " + currMax + " Length: " + randomLength);
-        polygon += polarToCartesian(centerXPos, centerYPos, randomLength, angle) + ",";
         angle += angleIncrement;
     }
-    console.log("Polygon: " + polygon);
-    var shape = globals.boardElement.polygon(polygon.substr(0, polygon.lastIndexOf(",")));
+    console.log("pathArray: " + pathArray);
+    pathString = "M" + pathArray[0];
+    for(index = 1; index < pathArray.length; index += 1) {
+        pathString += pathArray[index].toString();
+    }
+    pathString = pathString.substr(0, pathString.lastIndexOf(",")) + "Z";
+    console.log("pathString: " + pathString);
+    var shape = globals.boardElement.path(pathString);
         shape.attr({
         fill: "gray",
         stroke: "black"
     });
-    // for(index = 0; index < globals.marks.length; index += 1) {
-    //   var mark = globals.boardElement.text(globals.marks[index].x, globals.marks[index].y, index);
-    //   mark.attr({
-    //     fill: "red",
-    //     stroke: "black"
-    //   });
-    // }
 }
