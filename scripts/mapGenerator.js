7fc96a5c37d292d8da33db38c72035e2237ec412
ChexWarrior <aplehm@gmail.com>
2016-04-10 19:58:14 -0400

    Remove code to shrink borders and decrease border opacity

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -41,7 +41,7 @@ var mapGenerator = {
 
         for(i = 0; i < cells.length; i += 1) {
             currentCell = cells[i];
-            pathStr = "";
+            pathStr = '';
             onEdge = false;
             for(x = 0; x < currentCell.halfedges.length; x += 1) {
                 startX = currentCell.halfedges[x].getStartpoint().x;
@@ -49,62 +49,16 @@ var mapGenerator = {
                 endX = currentCell.halfedges[x].getEndpoint().x;
                 endY = currentCell.halfedges[x].getEndpoint().y;
 
-                //TODO: Force all cells to move edges closer to center by one, this
-                //will ensure that each cells border can be shown w/o overlap. Use
-                //each side positions relative to site point to determine calculations...
-                //Subtract the center coordinates from each edges point
-                // A negative y indicates it is above the current point
-                // A positive y indicates it is below the current point
-                // A negative x indicates it is to the left of the current point
-                // A positive x indicates it is to the right of the current point
-
-                //to the right
-                if(startX - currentCell.site.x > 0) {
-                    startX -= 1;
-                //to the left
-                } else if (startX - currentCell.site.x < 0) {
-                    startX += 1;
-                }
-
-                //to the right
-                if(endX - currentCell.site.x > 0) {
-                    endX -= 1;
-                //to the left
-                } else if (endX - currentCell.site.x < 0) {
-                    endX += 1;
-                }
-
-                //below
-                if(startY - currentCell.site.y > 0) {
-                    startY -= 1;
-                //above
-                } else if (startY - currentCell.site.y < 0) {
-                    startY += 1;
-                }
-
-                //below
-                if(endY - currentCell.site.y > 0) {
-                    endY -= 1;
-                //above
-                } else if (endY - currentCell.site.y < 0) {
-                    endY += 1;
-                }
-
                 pathStr += ((x === 0) ? "M" : "L") + startX + "," + startY 
                     + "L" + endX + "," + endY;
             }
 
             cellPath = this.paper.path(pathStr);
 
-            //draw site point
-            sitePoint = this.paper.circle(currentCell.site.x, currentCell.site.y, 2);
-            sitePoint.attr({
-                fill: '#D61515'
-            });
-
             cellPath.attr({
                 stroke: "#000000",
-                //'stroke-width': 2,
+                'stroke-opacity': .5,
+                'stroke-width': 2,
                 fill: onEdge ? "#2F4FED" : "#6B4B2A"
             });
 
